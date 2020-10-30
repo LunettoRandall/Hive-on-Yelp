6f1afad2c4e17b45a271e3396ee39006cd702202
Randall Lunetto <rlunetto@gmail.com>
2020-10-30 17:26:26 -0300

    Update 3_post.sh

--- tip_sentiment/tip_sentiment_trial_1/3_post.sh
+++ tip_sentiment/tip_sentiment_trial_1/3_post.sh
@@ -3,9 +3,9 @@
 hdfs dfs -get hdfs://mycluster/apps/hive/warehouse/rlunett.db/tip_sentiment
 
 #Convert 000000_0 output file into .csv file
-cat 0000* > tip_sentiment.csv
+cat 000000_0 > tip_sentiment.csv
 
-#Download file onto local bash from server
-scp malam@129.150.79.19:/home/malam/yelp/results/tip_sentiment/tip_sentiment_trial_1/tip_sentiment.csv ~/
+#Download file from server home directory into local disk home directory
+scp malam@129.150.79.19:/home/malam/tip_sentiment.csv ~/
 
 
