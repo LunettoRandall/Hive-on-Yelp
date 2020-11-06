981eaa357ef4967b9b89698a72e798681327698c
Randall Lunetto <rlunetto@gmail.com>
2020-11-05 23:48:25 -0300

    Updated path and finalized tip_sentiment summary

--- tip_sentiment/tip_sentiment_final/output.sh
+++ tip_sentiment/tip_sentiment_final/output.sh
@@ -1,9 +1,10 @@
 cd yelp/results/tip_sentiment/
 
-hdfs dfs -get /user/malam/yelp/results/tip_sentiment_final
+hdfs dfs -get /user/malam/yelp/results/tip_sentiment_summary
 
-cd yelp/results/tip_sentiment/tip_sentiment_final
+cd yelp/results/tip_sentiment/tip_sentiment_summary
 
-cat 00* > tip_sentiment2.csv
+cat 00* > tip_sentiment.csv
 
-scp malam@129.150.79.19:/home/malam/yelp/results/tip_sentiment/tip_sentiment_final/tip_sentiment_final.csv ~/
+scp malam@129.150.79.19:/home/malam/yelp/results/tip_sentiment/tip_sentiment_summary/tip_sentiment.csv ~/
+
